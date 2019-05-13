469ae31f3a76a200e77efaa84e462ae1f7da3926
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2019-05-13 10:59:47 -0600

    Added Files

---
+++ Assets/Scripts/Scene/Floor/FloorMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMovement : MonoBehaviour
+{
+    public float Speed;
+    private Vector3 endPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endPosition = transform.position;
+        endPosition.x -= Speed;
+        transform.position = Vector3.Lerp(transform.position, endPosition, 1 * Time.deltaTime);
+        Speed += 1 * Time.deltaTime;
+    }
+}
