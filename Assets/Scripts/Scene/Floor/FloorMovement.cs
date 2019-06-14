30380f1d0ddf3be66840713baa250d8400cc31f2
Laptop <StevOOOOOOOOO0@YAHOO.com>
2019-06-13 22:58:18 -0600

    Animation and floor movement

--- Assets/Scripts/Scene/Floor/FloorMovement.cs
+++ Assets/Scripts/Scene/Floor/FloorMovement.cs
@@ -8,6 +8,7 @@ public class FloorMovement : MonoBehaviour
     public FloatData FloorSpeed;
     private Vector3 endPosition;
     private Vector3 position;
+    public float DeactivatePostition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,7 @@ public class FloorMovement : MonoBehaviour
         endPosition.x -= FloorSpeed.Value;
         position = Vector3.Lerp(position, endPosition, 1 * Time.deltaTime);
         transform.position = position;
-        if (transform.position.x < -22f)
+        if (transform.position.x < DeactivatePostition)
             Destroy(gameObject);
     }
 }
