1ed9bde6c5e03e2b61b863a55595237d49f82cc4
Laptop <StevOOOOOOOOO0@YAHOO.com>
2019-06-18 10:02:39 -0600

    added hand script and worked on scene creation

---
+++ Assets/Scripts/Hand/HandBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandBehavior : MonoBehaviour {
+
+	public Transform FollowThis;
+	public Transform Follower;
+	private Transform positionUpdater;
+	
+	// Update is called once per frame
+	void Update () {
+		positionUpdater.position = FollowThis.position;
+		positionUpdater.position.Set(0, positionUpdater.position.y, 0);
+		Follower.position = positionUpdater.position;
+	}
+}
