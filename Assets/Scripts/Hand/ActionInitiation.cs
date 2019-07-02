34cdac355c4204ad91feca09772e39385dc66c1e
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2019-07-02 02:58:41 -0600

    scripts for the hand activation

--- Assets/Scripts/Hand/ActionInitiation.cs
+++ Assets/Scripts/Hand/ActionInitiation.cs
@@ -1,30 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.AccessControl;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 
 public class ActionInitiation : MonoBehaviour
 {
 
-	public FloatData Time;
+	public FloatData TimeValue;
 	private int randNumber;
 	public ListData Objects;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		Time.Value = -7;
-		Time.Timer = true;
+		TimeValue.Value = -7;
+		TimeValue.Timer = true;
+		for(int i = 0; i < (Objects.Value.Count); i++)
+		{
+			Instantiate(Objects.Value[i]);
+			Objects.Value[i].SetActive(false);
+			Debug.Log("is it active? - " + Objects.Value[i].activeInHierarchy);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Time.Value >= 3)
+		if (TimeValue.Timer)
+			TimeValue.Value += 1 * Time.deltaTime;
+	
+		if (TimeValue.Value >= 3 && TimeValue.Timer)
 		{
 			randNumber = Random.Range(0, (Objects.Value.Count - 1));		//Create New Hand choosing between four options
+			Debug.Log("in i now equals: " + randNumber);
 			Objects.Value[randNumber].SetActive(true);
-			Time.Timer = false;
+			Debug.Log("is it active? - " + Objects.Value[randNumber].activeInHierarchy);
+			TimeValue.Timer = false;
 		}
 	}
 }
---
+++ Assets/Scripts/Hand/WarningInitiation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningInitiation : MonoBehaviour
+{
+
+	public float TimeActive;
+	private float TimeActiveRemember;
+	public float XMin;
+	public float XMax;
+	public float YMin;
+	public float YMax;
+	
+	// Use this for initialization
+	void OnEnable ()
+	{
+		transform.position.Set(Random.Range(XMin, XMax), Random.Range(YMin, YMax), 0);				// give the sprite a random location within the range
+		TimeActiveRemember = TimeActive;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		TimeActive -= 1 * Time.deltaTime;
+		if (TimeActive <= 0)
+		{
+			TimeActive = TimeActiveRemember;
+			gameObject.SetActive(false);
+		}
+	}
+}
